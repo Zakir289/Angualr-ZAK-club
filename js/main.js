3abd60284050f1e65145bc2b3e8b19c2497e49cc
zakir <zakir.elahi@bankbazaar.com>
2015-07-10 02:23:55 +0530

    Updating zakir's club

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+var app = angular.module('AngularTutorial', [
+  'ngRoute'
+]);
+
+
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
+    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+
+app.controller('PageCtrl', function (/*written for name sake */) {
+  console.log("controller for name sake.");
+
+  
+});
